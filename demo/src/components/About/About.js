bef2e9a0fb2ef0f8b0fecbbee52351247aaf7536
Manisha11sep <er.manisha11sep@gmail.com>
2018-03-27 12:29:36 -0700

    exercise completed

--- demo/src/components/About/About.js
+++ demo/src/components/About/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
 
 export default function About(props) {
   return (
     <div>
       <h1>This is the About component</h1>
-      <h3>FAQ</h3>
-      <h3>Company</h3>
+     
+          
+      <h3>FAQ  <Link to ="/about/FAQ" > FAQ </Link> </h3>
+      <h3>Company <Link to ="/about/Company"> Company </Link></h3>
       {props.children}
     </div>
   )
--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
-
+import Home from './Home/Home';
+import About from './About/About';
+import People from './People/People';
+import {Route, Link} from 'react-router-dom';
+import FAQ from './About/FAQ/FAQ';
+import Company from './About/Company/Company';
+import Character from './Character/Character';
 class App extends Component {
   render() {
     return (
       <div>
         This is App.js
+        <nav>
+          <Link to = "/" >Home </Link>
+          <Link to ="/about"> About  </Link>
+          <Link to ="/people"> People  </Link>
+          </nav>
+        <Route exact path ="/" component ={Home} />
+        <Route path ="/about" render ={() => <About>
+        {
+          <Route path="/about/FAQ" component ={FAQ} />
+          // <Route path="/about/Company" component ={Company} />
+        }
+        </About> }/>
+        <Route exact path="/people" component ={People} />
+        <Route path ="/people/:id" component={Character} />
+        
+
       </div>
     );
   }
